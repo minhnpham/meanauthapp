9d540f9d6d0493fb05bad66729e4c82388311d43
Minh Pham <minh.nhu.pham@gmail.com>
2018-11-15 22:18:31 +1100

    Updated for passport v3+

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const config = require('../config/database');
 
 module.exports = function(passport) {
   let opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
   opts.secretOrKey = config.secret;
   passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     console.log(jwt_payload);
